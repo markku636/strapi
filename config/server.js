8905537647fb5bf533692176025654609e3b16b4
Mark.K <a4756830@gmail.com>
2022-09-12 21:52:25 +0800

    update

--- config/server.js
+++ config/server.js
@@ -1,7 +1,24 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  host: env("HOST", "0.0.0.0"),
+  port: env.int("PORT", 1337),
   app: {
-    keys: env.array('APP_KEYS'),
+    keys: env.array("APP_KEYS"),
+  },
+  vercel: {
+    // Required
+    token: "9PpC8FtFMPXZY9eapq4poz6E",
+    // Required
+    projectId: "prj_P6TYqTooLybxMHhwoUi5npimDepm",
+    // Optional (required if you use teams)
+    // teamId: env("VERCEL_TEAM_ID"),
+    // Required (hooks)
+    triggers: {
+      production: "https://api.vercel.com/v1/integrations/deploy/prj_P6TYqTooLybxMHhwoUi5npimDepm/Z7zGvMDREX",
+    },
+    // Optional
+    config: {
+      // Number of latest deployments
+      deployments: 10,
+    },
   },
 });
