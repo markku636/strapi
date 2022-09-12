1debf6e54f06c7130a32269cb525f850a9948f8a
Mark.K <a4756830@gmail.com>
2022-09-12 22:44:56 +0800

    update

--- config/plugins.js
+++ config/plugins.js
@@ -1,17 +1,11 @@
 module.exports = ({ env }) => ({
-  host: env("HOST", "0.0.0.0"),
-  port: env.int("PORT", 1337),
-  app: {
-    keys: env.array("APP_KEYS"),
-  },
   "vercel-deploy": {
     enabled: true,
     config: {
-      deployHook:
+    deployHook:
         "https://api.vercel.com/v1/integrations/deploy/prj_P6TYqTooLybxMHhwoUi5npimDepm/Z7zGvMDREX",
       apiToken: "9PpC8FtFMPXZY9eapq4poz6E",
       appFilter: "strapi",
-      teamFilter: "",
       roles: ["strapi-super-admin", "strapi-editor", "strapi-author"],
     },
   },
