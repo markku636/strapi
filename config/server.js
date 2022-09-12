abd6047a5482c08fa188f1034a5c3bc758498312
Mark.K <a4756830@gmail.com>
2022-09-12 21:57:35 +0800

    update

--- config/server.js
+++ config/server.js
@@ -9,6 +9,7 @@ module.exports = ({ env }) => ({
     token: "9PpC8FtFMPXZY9eapq4poz6E",
     // Required
     projectId: "prj_P6TYqTooLybxMHhwoUi5npimDepm",
+    
     // Optional (required if you use teams)
     // teamId: env("VERCEL_TEAM_ID"),
     // Required (hooks)
